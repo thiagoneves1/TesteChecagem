2ec428120950332df0f18201cd0fba82e1845f6c
Thiago <ttn.passos@gmail.com>
2014-12-03 22:50:10 -0200

    refatoração

--- app/src/main/java/br/com/testestec/testes/MyActivity.java
+++ app/src/main/java/br/com/testestec/testes/MyActivity.java
@@ -6,14 +6,57 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import br.com.testestec.moduloChecagem.Controle;
 
 
 public class MyActivity extends ActionBarActivity {
-ListView listView ;
 
-    String json="{\"checklists\":[{\"App\":\"X\",\"id\":1,\"tituloChecagem\":\"Entrega do Carro do José\",\"categorias\":[{\"nomeCategoria\":\"Manuais e Serviços\",\"itensDaCategoria\":[{\"id\":1,\"tituloItem\":\"Entrega do Manual do Proprietário\",\"perguntas\":[{\"id\":1,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":2,\"tipo\":\"texto-livre\"}]},{\"id\":2,\"tituloItem\":\"Entrega da Chave reserva\",\"perguntas\":[{\"id\":3,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":4,\"tipo\":\"texto-livre\"}]}]},{\"nomeCategoria\":\"Documentos\",\"itensDaCategoria\":[{\"id\":3,\"tituloItem\":\"Informação sobre a Autorização Provisória para circulação\",\"perguntas\":[{\"id\":5,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":6,\"tipo\":\"texto-livre\"}]},{\"id\":4,\"tituloItem\":\"Avaliação das condições externas do veículo (lataria)\",\"perguntas\":[{\"id\":7,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":8,\"tipo\":\"texto-livre\",\"opcional\":\"true\"},{\"id\":9,\"tipo\":\"alternativas-lista\",\"opcional\":\"true\",\"opcoes\":[{\"texto\":\"V1+\",\"resposta\":\"1\"},{\"texto\":\"V1\",\"resposta\":\"2\"},{\"texto\":\"V2\",\"resposta\":\"3\"},{\"texto\":\"V3\",\"resposta\":\"4\"}],\"visivel-se\":{\"pergunta\":5,\"resposta\":\"N-A\",\"valor\":\"0\"}},{\"id\":10,\"tipo\":\"foto\",\"opcional\":\"true\"}]}]}]},{\"App\":\"Z\",\"id\":2,\"tituloChecagem\":\"Entrega do Carro do João\",\"categorias\":[{\"nomeCategoria\":\"Manuais e Serviços do João\",\"itensDaCategoria\":[{\"id\":1,\"tituloItem\":\"Entrega do Manual do Proprietário do João\",\"perguntas\":[{\"id\":1,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":2,\"tipo\":\"texto-livre\"}]},{\"id\":2,\"tituloItem\":\"Entrega da Chave reserva do João\",\"perguntas\":[{\"id\":3,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":4,\"tipo\":\"texto-livre\"}]}]},{\"nomeCategoria\":\"Documentos do João\",\"itensDaCategoria\":[{\"id\":3,\"tituloItem\":\"Informação sobre a Autorização Provisória para circulação do João\",\"perguntas\":[{\"id\":5,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":6,\"tipo\":\"texto-livre\"}]},{\"id\":4,\"tituloItem\":\"Avaliação das condições externas do veículo (lataria) do João\",\"perguntas\":[{\"id\":7,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":8,\"tipo\":\"texto-livre\",\"opcional\":\"true\"},{\"id\":9,\"tipo\":\"alternativas-lista\",\"opcional\":\"true\",\"opcoes\":[{\"texto\":\"V1+\",\"resposta\":\"1\"},{\"texto\":\"V1\",\"resposta\":\"2\"},{\"texto\":\"V2\",\"resposta\":\"3\"},{\"texto\":\"V3\",\"resposta\":\"4\"}],\"visivel-se\":{\"pergunta\":5,\"resposta\":\"N-A\",\"valor\":\"0\"}},{\"id\":10,\"tipo\":\"foto\",\"opcional\":\"true\"}]}]}]}]}";
+
+    String json="{\"checklists\":[{\"App\":\"X\",\"id\":1,\"tituloChecagem\":\"Entrega do Carro do " +
+            "José\",\"categorias\":[{\"nomeCategoria\":\"Manuais e Serviços\",\"itensDaCategoria\"" +
+            ":[{\"id\":1,\"tituloItem\":\"Entrega do Manual do Proprietário\",\"perguntas\"" +
+            ":[{\"id\":1,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"" +
+            "resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"" +
+            "resposta\":\"0\"}]},{\"id\":2,\"tipo\":\"texto-livre\"}]},{\"id\":2,\"tituloItem\":\"" +
+            "Entrega da Chave reserva\",\"perguntas\":[{\"id\":3,\"tipo\":\"alternativas-radio\",\"" +
+            "opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"}," +
+            "{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":4,\"tipo\":\"texto-livre\"}]}]}," +
+            "{\"nomeCategoria\":\"Documentos\",\"itensDaCategoria\":[{\"id\":3,\"tituloItem\":\"" +
+            "Informação sobre a Autorização Provisória para circulação\",\"perguntas\":" +
+            "[{\"id\":5,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"" +
+            "resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"" +
+            "resposta\":\"0\"}]},{\"id\":6,\"tipo\":\"texto-livre\"}]},{\"id\":4,\"tituloItem\":\"" +
+            "Avaliação das condições externas do veículo (lataria)\",\"perguntas\":" +
+            "[{\"id\":7,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"" +
+            "resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"" +
+            "resposta\":\"0\"}]},{\"id\":8,\"tipo\":\"texto-livre\",\"opcional\":\"true\"},{\"id\":9,\"" +
+            "tipo\":\"alternativas-lista\",\"opcional\":\"true\",\"opcoes\":[{\"texto\":\"V1+\",\"" +
+            "resposta\":\"1\"},{\"texto\":\"V1\",\"resposta\":\"2\"},{\"texto\":\"V2\",\"" +
+            "resposta\":\"3\"},{\"texto\":\"V3\",\"resposta\":\"4\"}],\"visivel-se\":{\"pergunta\":5,\"" +
+            "resposta\":\"N-A\",\"valor\":\"0\"}},{\"id\":10,\"tipo\":\"foto\",\"" +
+            "opcional\":\"true\"}]}]}]},{\"App\":\"Z\",\"id\":2,\"tituloChecagem\":\"" +
+            "Entrega do Carro do João\",\"categorias\":[{\"nomeCategoria\":\"Manuais e Serviços do João\",\"" +
+            "itensDaCategoria\":[{\"id\":1,\"tituloItem\":\"Entrega do Manual do Proprietário do João\",\"" +
+            "perguntas\":[{\"id\":1,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"" +
+            "resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"" +
+            "resposta\":\"0\"}]},{\"id\":2,\"tipo\":\"texto-livre\"}]},{\"id\":2,\"tituloItem\":\"" +
+            "Entrega da Chave reserva do João\",\"perguntas\":[{\"id\":3,\"tipo\":\"alternativas-radio\",\"" +
+            "opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"" +
+            "texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":4,\"tipo\":\"texto-livre\"}]}]},{\"" +
+            "nomeCategoria\":\"Documentos do João\",\"itensDaCategoria\":[{\"id\":3,\"tituloItem\":\"" +
+            "Informação sobre a Autorização Provisória para circulação do João\",\"" +
+            "perguntas\":[{\"id\":5,\"tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"" +
+            "resposta\":\"1\"},{\"texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"" +
+            "resposta\":\"0\"}]},{\"id\":6,\"tipo\":\"texto-livre\"}]},{\"id\":4,\"tituloItem\":\"" +
+            "Avaliação das condições externas do veículo (lataria) do João\",\"perguntas\":[{\"id\":7,\"" +
+            "tipo\":\"alternativas-radio\",\"opcoes\":[{\"texto\":\"OK\",\"resposta\":\"1\"},{\"" +
+            "texto\":\"N-OK\",\"resposta\":\"2\"},{\"texto\":\"N-A\",\"resposta\":\"0\"}]},{\"id\":8,\"tipo\":\"" +
+            "texto-livre\",\"opcional\":\"true\"},{\"id\":9,\"tipo\":\"alternativas-lista\",\"opcional\":\"true\",\"" +
+            "opcoes\":[{\"texto\":\"V1+\",\"resposta\":\"1\"},{\"texto\":\"V1\",\"resposta\":\"2\"},{\"" +
+            "texto\":\"V2\",\"resposta\":\"3\"},{\"texto\":\"V3\",\"resposta\":\"4\"}],\"visivel-se\":{\"" +
+            "pergunta\":5,\"resposta\":\"N-A\",\"valor\":\"0\"}},{\"id\":10,\"tipo\":\"foto\",\"opcional\":\"true\"}]}]}]}]}";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +65,17 @@ ListView listView ;
         boolean inseriu = controle.insereJson(json);//mudar e passar um object JSON ?
 
         if(inseriu) {
-            Log.i("json inserido com sucesso ", String.valueOf(inseriu));
+            Log.i(String.valueOf(getApplicationContext()),"json inserido com sucesso");
         }
 
-            LinearLayout linearLayout = controle.getLayout();
+            LinearLayout linearLayout = controle.montaLayout();
             setContentView(linearLayout);//mostra o layout com dados existentes
-
 
 //        setContentView(R.layout.activity_my);
 //        getApplicationContext().deleteDatabase("bancoModulo");//usado para deletar o banco em testes
 
 
-//        String nomeApp = "X"; //OU Z COM A STRING MODELO, ACIMA
+//        String nomeApp = "X"; //ou Z com a string montada acima
 //        JSONObject jsonFinal = controle.obterJsonRespostas(nomeApp);
 //        Log.i("jsonFinal  ", jsonFinal.toString());
     }
